62033065f4344875f04cdfb1a48f602bd991229e
Rodrigo Mendes <rcmendes80@gmail.com>
2021-02-07 15:51:29 -0500

    added comments.

--- beer_api/app.py
+++ beer_api/app.py
@@ -4,5 +4,9 @@ from beer_api.presentation import api
 app = Flask(__name__)
 api.init_app(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+# TODO Add logs
+# TODO add error handlers
